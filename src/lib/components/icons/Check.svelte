308efb830988d7791661565dd4317fb424b13bda
Robert Jolly <robert.jolly@gsa.gov>
2025-03-17 07:38:21 -0600

    removing bad aria-labels, related to #414

--- src/lib/components/icons/Check.svelte
+++ src/lib/components/icons/Check.svelte
@@ -10,6 +10,7 @@
 	stroke-width={strokeWidth}
 	stroke="currentColor"
 	class={className}
+	aria-hidden="true"
 >
 	<path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
 </svg>
