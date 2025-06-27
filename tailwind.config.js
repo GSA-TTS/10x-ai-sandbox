d79ca7e80d373f15c80640e9d73f5663eff7f92a
LaTeshaDRichardson <198785486+lrichardson-work@users.noreply.github.com>
2025-06-27 16:47:28 -0700

    Add npm color token package, import css and update tailwind config, and applied some new color tokens to sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 import typography from '@tailwindcss/typography';
+import '@gsa-tts/graymatter-style-tokens/dist/css/token-list.json'
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -7,6 +8,64 @@ export default {
 	theme: {
 		extend: {
 			colors: {
+				colors: {
+					white: 'ai-color-white": "#ffffff',
+  				black: 'ai-color-black": "#000000'
+				},
+				neutral:{
+					0: 'var(--ai-color-neutral-0)', // white
+					50: 'var(--ai-color-neutral-50)',
+					100: 'var(--ai-color-neutral-100)',
+					200: 'var(--ai-color-neutral-200)',
+					300: 'var(--ai-color-neutral-300)',
+					400: 'var(--ai-color-neutral-400)',
+					500: 'var(--ai-color-neutral-500)',
+					600: 'var(--ai-color-neutral-600)',
+					700: 'var(--ai-color-neutral-700)',
+					800: 'var(--ai-color-neutral-800)',
+					850: 'var(--ai-color-neutral-850)',
+					900: 'var(--ai-color-neutral-900)',
+					950: 'var(--ai-color-neutral-950)',
+					1000: 'var(--ai-color-neutral-1000)'
+				},
+				steel:{
+					50: 'var(--ai-color-steel-50)', 
+					100: 'var(--ai-color-steel-100)',
+					200: 'var(--ai-color-steel-200)',
+					300: 'var(--ai-color-steel-300)',
+					400: 'var(--ai-color-steel-400)',
+					500: 'var(--ai-color-steel-500)',
+					600: 'var(--ai-color-steel-600)',
+					700: 'var(--ai-color-steel-700)',
+					800: 'var(--ai-color-steel-800)',
+					900: 'var(--ai-color-steel-900)',
+				},
+				blue:{
+					50: 'var(--ai-color-blue-50)', 
+					100: 'var(--ai-color-blue-100)',
+					200: 'var(--ai-color-blue-200)',
+					300: 'var(--ai-color-blue-300)',
+					400: 'var(--ai-color-blue-400)',
+					500: 'var(--ai-color-blue-500)',
+					600: 'var(--ai-color-blue-600)',
+					700: 'var(--ai-color-blue-700)',
+					800: 'var(--ai-color-blue-800)',
+					900: 'var(--ai-color-blue-900)',
+				},
+				green:{
+					50: 'var(--ai-color-green-50)', 
+					100: 'var(--ai-color-green-10)', 
+					200: 'var(--ai-color-green-200)', 
+					300: 'var(--ai-color-green-300)', 
+					400: 'var(--ai-color-green-400)', 
+					500: 'var(--ai-color-green-500)', 
+					600: 'var(--ai-color-green-600)', 
+					700: 'var(--ai-color-green-700)', 
+					800: 'var(--ai-color-green-800)', 
+					900: 'var(--ai-color-green-900)', 
+				},
+
+				//existing tailwind gray - will delete after color updates
 				gray: {
 					50: '#f9f9f9',
 					100: '#ececec',
